0409eff05fc00c50f82175984971493e1aa07abd
sumyiren <sumyiren@gmail.com>
2019-06-08 03:29:19 +0100

    [Test35] move back to 3 nSellers

--- drqnMLEngine/Trainer.py
+++ drqnMLEngine/Trainer.py
@@ -9,7 +9,7 @@ from drqnMLEngine.DRQNseller import dqrnSeller
 class Trainer(object):
 
     def __init__(self, args):
-        self.nSellers = 2
+        self.nSellers = 3
         self.reward_record = []
 
         self.n_agents = self.nSellers
